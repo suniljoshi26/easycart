a1ae8d1159ed24e53dc0ec95e9691b4c2e560e2e
Sunil Joshi <joshisunil2601@gmail.com>
2022-09-22 14:34:33 +0530

    cart bug fixed

--- src/AddCartButton.jsx
+++ src/AddCartButton.jsx
@@ -3,14 +3,18 @@ import React from "react";
 function AddCartButton(props) {
   let themclass;
   if (props.them == "highlight") {
-    themclass = "text-white bg-red-500 text-xl font-samibold rounded-md";
+    themclass = "text-white bg-red-500 hover:bg-red-300 rounded-md";
   }
   if (props.them === "secondary") {
-    themclass = "bg-red-300 text-gray-400 text-xl font-samibold rounded-md";
+    themclass = "bg-red-300 text-gray-400  hover:bg-red-500 rounded-md";
   }
   return (
     <div>
-      <button className={"px-24 p-3 border border-gray-400 " + themclass}>
+      <button
+        className={
+          "px-12 p-2 border text-xl font-samibold border-gray-400 " + themclass
+        }
+      >
         {props.children}
       </button>
     </div>
