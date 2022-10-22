26324f80e2676524d7bc17b5238ab1e7dc3c4c7c
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-22 19:54:35 +0530

    cart page phone view

--- src/Cart/CartListPage.jsx
+++ src/Cart/CartListPage.jsx
@@ -61,16 +61,16 @@ const CartListPage = ({ cart, updateCart }) => {
           />
         );
       })}
-      <div className="flex justify-between border border-gray-300 px-4 py-2 bg-white ">
+      <div className="sm:flex justify-between border border-gray-300 px-4 py-2 bg-white ">
         <div className="space-x-1">
           {" "}
-          <div className="flex space-x-2">
+          <div className=" flex space-x-2">
             <Input />
             <Button>APPLY COUPON</Button>
           </div>
         </div>
         <div>
-          <Button onClick={handleUpdateCart}>update cart</Button>
+          <Button onClick={handleUpdateCart}>UPDATE CART </Button>
         </div>
       </div>
     </div>
